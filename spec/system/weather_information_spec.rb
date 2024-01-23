f88782fb4b2e6c8c5eaf938ebfaa98a8860fee53
Alex Malik <alexmalik60@gmail.com>
2024-01-23 09:15:00 -0700

    test: convert controller spec to request spec

--- spec/system/weather_information_spec.rb
+++ spec/system/weather_information_spec.rb
@@ -1,48 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe WeatherInformationController, type: :controller do
-	render_views
-	describe '#index' do
-		it 'returns 200 status' do
-			get :index
+RSpec.describe 'WeatherInformation', type: :request do
+	context 'get /index' do
+		before do
+			get '/'
+		end
 
+		it 'should return 200 ok' do
 			expect(response.status).to be 200
+		end
+
+		it 'should render the index template' do
+			expect(response.body).to match(/<h1 class=\"mb-8 text-3xl text-center\">Weather App<\/h1>/)
 		end
 	end
 
 	describe '#data' do
+		let(:open_weather_key) { Rails.application.credentials.open_weather_key }
+
+		let(:uri) do
+			"https://api.openweathermap.org/data/2.5/weather?" \
+			"appid=#{open_weather_key}&lat=#{form_params[:latitude]}&lon=#{form_params[:longitude]}"
+		end
+
+		let(:form_params) do
+			{
+				location: "Goa, India", 
+				latitude: "15.2993265", 
+				longitude: "74.12399599999999"
+			}
+		end
+
 		context 'with no location params' do
 			it 'redirects to index page with approriate message' do
-				post :data
+				post data_path
 
 				expect(response.status).to be 302
 				expect(response).to redirect_to(root_path)
 				expect(flash[:notice]).to eq('Please input a valid location')
+				expect(WebMock).not_to have_requested(:get, uri)
 			end
 		end
 
 		context 'with location params' do
-			subject { post :data, params: form_params, as: :turbo_stream }
-			let(:open_weather_key) { Rails.application.credentials.open_weather_key }
-
-			let(:uri) do
-				"https://api.openweathermap.org/data/2.5/weather?" \
-				"appid=#{open_weather_key}&lat=#{form_params[:latitude]}&lon=#{form_params[:longitude]}"
-			end
+			subject { post data_path, params: form_params, as: :turbo_stream }
 
 			let(:request_headers) do
 				{
 					'Accept'=>'*/*',
 					'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
 					'User-Agent'=>'Ruby'
-				}
-			end
-
-			let(:form_params) do
-				{
-					location: "Goa, India", 
-					latitude: "15.2993265", 
-					longitude: "74.12399599999999"
 				}
 			end
 
